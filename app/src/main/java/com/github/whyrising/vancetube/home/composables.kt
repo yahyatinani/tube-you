44e8a80945a24a68239ca37b470f4c5f23442a8c
Yahya <47183730+whyrising@users.noreply.github.com>
2022-08-06 16:16:05 +0100

    [WIP]

--- app/src/main/java/com/github/whyrising/vancetube/home/composables.kt
+++ app/src/main/java/com/github/whyrising/vancetube/home/composables.kt
@@ -2,46 +2,66 @@ package com.github.whyrising.vancetube.home
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraphBuilder
-import com.github.whyrising.recompose.dispatch
-import com.github.whyrising.recompose.subscribe
-import com.github.whyrising.recompose.w
-import com.github.whyrising.vancetube.about.about
-import com.github.whyrising.vancetube.base.base
 import com.github.whyrising.vancetube.base.regBaseSubs
 import com.github.whyrising.vancetube.initAppDb
 import com.github.whyrising.vancetube.ui.anim.enterAnimation
 import com.github.whyrising.vancetube.ui.anim.exitAnimation
 import com.github.whyrising.vancetube.ui.theme.VanceTheme
-import com.github.whyrising.y.core.v
 import com.google.accompanist.navigation.animation.composable
+
+data class VideoItem(
+  val title: String,
+  val thumbnail: Any?,
+  val length: String,
+  val channelName: String,
+  val channelAvatar: String,
+  val viewsCount: String,
+  val releaseTime: String
+)
 
 @Composable
 fun Home(modifier: Modifier = Modifier) {
+  val homeVideos = listOf(
+    VideoItem(
+      title = "Cheap and Easy VPN in 5 Minutes!",
+      thumbnail = null,
+      length = "Jhon Doe",
+      channelName = "",
+      channelAvatar = "",
+      viewsCount = "33K views",
+      releaseTime = "2 weeks ago"
+    )
+  )
   Surface(modifier = modifier.fillMaxSize()) {
-    Column(
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center
-    ) {
-      Button(
-        onClick = { dispatch(v(home.inc_count)) }
-      ) {
-        Text(text = subscribe<String>(v(home.btn_count_name)).w())
-      }
-      Button(
-        onClick = { dispatch(v(base.navigate, about.about_panel)) }
-      ) {
-        Text(text = "About")
+    LazyColumn {
+      items(homeVideos) { video: VideoItem ->
+        Column {
+          Image(
+            modifier = Modifier
+              .fillParentMaxWidth()
+              .height(200.dp),
+            imageVector = Icons.Filled.PlayArrow,
+            contentDescription = "thumbnail",
+          )
+          Row {
+
+          }
+        }
       }
     }
   }

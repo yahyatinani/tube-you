fc7dd3d974be1293382c24fc37ae41b7e4077811
Yahya <47183730+whyrising@users.noreply.github.com>
2022-09-18 15:10:47 +0100

    WIP on navigateBottomBar macrobenchmark

--- benchmark/src/main/java/com/github/whyrising/vancetube/benchmark/BaselineProfileBenchmark.kt
+++ benchmark/src/main/java/com/github/whyrising/vancetube/benchmark/BaselineProfileBenchmark.kt
@@ -2,10 +2,13 @@ package com.github.whyrising.vancetube.benchmark
 
 import androidx.benchmark.macro.BaselineProfileMode
 import androidx.benchmark.macro.CompilationMode
+import androidx.benchmark.macro.FrameTimingMetric
 import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.Until
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,9 +24,11 @@ class BaselineProfileBenchmark {
       metrics = listOf(StartupTimingMetric()),
       iterations = 10,
       startupMode = StartupMode.COLD,
-      compilationMode = compilationMode
+      compilationMode = compilationMode,
+      setupBlock = {
+        pressHome()
+      }
     ) {
-      pressHome()
       startActivityAndWait()
     }
   }
@@ -36,5 +41,33 @@ class BaselineProfileBenchmark {
   @Test
   fun startupBaselineProfile() {
     startup(CompilationMode.Partial(BaselineProfileMode.Require))
+  }
+
+  @Test
+  fun navigateBottomBar() {
+    val listTag = "popular_videos_list"
+    benchmarkRule.measureRepeated(
+      packageName = "com.github.whyrising.vancetube",
+      metrics = listOf(FrameTimingMetric()),
+      iterations = 10,
+      startupMode = StartupMode.COLD,
+      compilationMode = CompilationMode.Partial(BaselineProfileMode.Require),
+      setupBlock = {
+        pressHome()
+        startActivityAndWait()
+        device.wait(Until.hasObject(By.res(listTag)), 20_000)
+        val contentList = device.findObject(By.res(listTag))
+        contentList.wait(Until.hasObject(By.res("video")), 5000)
+
+        // Navigate to Library tab
+        device.findObject(By.text("Library")).click()
+        device.waitForIdle()
+        device.wait(Until.hasObject(By.res("sdfsdf")), 2_000)
+      }
+    ) {
+      // Navigate back to Home tab
+      device.findObject(By.text("Home")).click()
+      device.wait(Until.hasObject(By.res(listTag)), 20_000)
+    }
   }
 }

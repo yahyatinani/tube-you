8b5c466f8ab9edd9367d01f4e332d19deb8ce31b
Yahya <47183730+whyrising@users.noreply.github.com>
2022-10-04 19:15:30 +0100

    Add :core:keywords java module

---
+++ modules/core/keywords/src/main/java/com/github/whyrising/vancetube/modules/core/keywords/home.kt
@@ -0,0 +1,15 @@
+package com.github.whyrising.vancetube.modules.core.keywords
+
+enum class home {
+  route,
+  panel,
+  state,
+  view_model,
+  set_popular_vids,
+  refresh,
+  load_popular_videos,
+  go_top_list,
+  popular_vids;
+
+  override fun toString(): String = ":${javaClass.simpleName}/$name"
+}

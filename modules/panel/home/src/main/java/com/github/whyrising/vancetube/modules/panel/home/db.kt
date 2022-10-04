a460ee3281c1060dab55ae8775ea7f52f2e5d7db
Yahya <47183730+whyrising@users.noreply.github.com>
2022-10-04 20:10:05 +0100

    Add documentation to home db schema

--- modules/panel/home/src/main/java/com/github/whyrising/vancetube/modules/panel/home/db.kt
+++ modules/panel/home/src/main/java/com/github/whyrising/vancetube/modules/panel/home/db.kt
@@ -3,3 +3,11 @@ package com.github.whyrising.vancetube.modules.panel.home
 import com.github.whyrising.y.core.collections.IPersistentMap
 
 typealias AppDb = IPersistentMap<Any, Any>
+
+/**
+ * Spec
+ *
+ * e.g:
+ * {:state       Loading
+ * :popular_vids (VideoData)}
+ */

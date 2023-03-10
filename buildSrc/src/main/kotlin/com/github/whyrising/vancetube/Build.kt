095031ce37e23b6ccc1be4b1a18e8a56de8cb9f7
Yahya Tinani <47183730+yahyatinani@users.noreply.github.com>
2023-03-10 21:32:36 +0100

    Update to Kotlin 1.8

--- buildSrc/src/main/kotlin/com/github/whyrising/vancetube/Build.kt
+++ buildSrc/src/main/kotlin/com/github/whyrising/vancetube/Build.kt
@@ -28,7 +28,7 @@ object Build {
   }
 
   object Versions {
-    const val COMPOSE_COMPILER = "1.3.2"
+    const val COMPOSE_COMPILER = "1.4.3"
     const val KOTLIN = "1.7"
     const val JVM = "1.8"
   }

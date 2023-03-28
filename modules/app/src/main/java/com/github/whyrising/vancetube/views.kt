4b6a05bb35d811da8177f9b70fd1b7b457681fe7
Yahya Tinani <47183730+yahyatinani@users.noreply.github.com>
2023-03-28 17:08:17 +0000

    Use NavigationBarDefaults.windowInsets for bottom navigation bar

--- modules/app/src/main/java/com/github/whyrising/vancetube/views.kt
+++ modules/app/src/main/java/com/github/whyrising/vancetube/views.kt
@@ -34,6 +34,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
@@ -321,7 +322,7 @@ fun VanceApp(
       bottomBar = {
         Surface(
           modifier = Modifier.windowInsetsPadding(
-            WindowInsets.safeDrawing.only(Horizontal + Bottom)
+            NavigationBarDefaults.windowInsets
           )
         ) {
           Box(contentAlignment = TopCenter) {

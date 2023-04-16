5c6a0d0fa8ad7e4fd1d2b19ab5dd0a6dc7be4a79
Yahya Tinani <47183730+yahyatinani@users.noreply.github.com>
2023-04-16 15:12:43 +0000

    Trim search input

--- modules/panel/common/src/main/java/com/github/whyrising/vancetube/modules/panel/common/events.kt
+++ modules/panel/common/src/main/java/com/github/whyrising/vancetube/modules/panel/common/events.kt
@@ -2,11 +2,12 @@ package com.github.whyrising.vancetube.modules.panel.common
 
 import com.github.whyrising.recompose.cofx.Coeffects
 import com.github.whyrising.recompose.cofx.injectCofx
-import com.github.whyrising.recompose.fx.BuiltInFx
+import com.github.whyrising.recompose.fx.BuiltInFx.fx
 import com.github.whyrising.recompose.ids.recompose.db
 import com.github.whyrising.recompose.regEventFx
 import com.github.whyrising.vancetube.modules.core.keywords.common
 import com.github.whyrising.vancetube.modules.core.keywords.home
+import com.github.whyrising.y.core.assocIn
 import com.github.whyrising.y.core.collections.PersistentVector
 import com.github.whyrising.y.core.get
 import com.github.whyrising.y.core.getIn
@@ -37,7 +38,7 @@ fun regCommonEvents() {
       "${appDb[common.api_endpoint]}/search/suggestions?q=$sq"
 
     m<Any, Any>().assoc(
-      BuiltInFx.fx,
+      fx,
       v(
         v(
           ktor.http_fx,
@@ -61,14 +62,19 @@ fun regCommonEvents() {
   ) { cofx, (_, searchQuery) ->
     if ((searchQuery as String).isEmpty()) return@regEventFx m()
 
-    val sq = searchQuery.replace(" ", "%20")
+    val trim = searchQuery.trim()
+    val sq = trim.replace(" ", "%20")
     val appDb = appDbBy(cofx)
     // TODO: &type=video, support a all types?
     val searchEndpoint = "${appDb[common.api_endpoint]}/search?q=$sq&type=video"
-
-    m<Any, Any>(db to appDb.assoc(common.is_search_bar_active, false)).assoc(
-      BuiltInFx.fx,
-      v(
+    val newDb = assocIn(
+      appDb,
+      l(appDb[common.active_navigation_item], common.search_bar, ":query"),
+      trim
+    )
+    m<Any, Any>(
+      db to newDb.assoc(common.is_search_bar_active, false),
+      fx to v(
         v(common.navigate_to, "search_query"),
         v(
           ktor.http_fx,

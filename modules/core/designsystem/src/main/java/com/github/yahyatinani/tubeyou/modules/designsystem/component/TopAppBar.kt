4be4c256508078bfe562e330c12b3664d415ed69
Yahya Tinani <47183730+yahyatinani@users.noreply.github.com>
2023-08-17 21:31:53 +0100

    Refactor

--- modules/core/designsystem/src/main/java/com/github/yahyatinani/tubeyou/modules/designsystem/component/TopAppBar.kt
+++ modules/core/designsystem/src/main/java/com/github/yahyatinani/tubeyou/modules/designsystem/component/TopAppBar.kt
@@ -8,12 +8,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarColors
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -21,6 +21,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -64,54 +65,57 @@ fun TyTopAppBar(title: String, actions: List<TopAppBarActionItem>) {
 @OptIn(ExperimentalMaterial3Api::class)
 fun TySearchBar(
   searchQuery: String,
-  onQueryChange: (String) -> Unit,
-  onSearch: (String) -> Unit,
-  isActive: Boolean,
-  onActiveChange: (Boolean) -> Unit,
-  clearInput: () -> Unit,
-  backPress: () -> Unit,
+  isSearchBarActive: Boolean,
+  colors: SearchBarColors = MaterialTheme.colorScheme.let { colorScheme ->
+    remember(colorScheme) { colorScheme.onSurface.copy(.6f) }.let {
+      SearchBarDefaults.colors(
+        inputFieldColors = SearchBarDefaults.inputFieldColors(
+          focusedPlaceholderColor = it,
+          unfocusedPlaceholderColor = it
+        )
+      )
+    }
+  },
+  onQueryChange: (updatedQuery: String) -> Unit,
+  onSearchClick: (searchedQuery: String) -> Unit,
+  onActiveChange: (isActive: Boolean) -> Unit,
+  onTrailingClick: () -> Unit,
+  onLeadingClick: () -> Unit,
   suggestions: List<String>,
-  colorScheme: ColorScheme,
-  onSuggestionSelected: (suggestion: String) -> Unit
+  onSuggestionClick: (suggestion: String) -> Unit
 ) {
   val focusRequester = FocusRequester()
-  val placeHolderColor = colorScheme.onSurface.copy(alpha = .6f)
   SearchBar(
     query = searchQuery,
     modifier = Modifier
       .fillMaxWidth()
       .focusRequester(focusRequester),
-    active = isActive,
+    active = isSearchBarActive,
     tonalElevation = 0.dp,
     shape = RoundedCornerShape(30.dp),
-    colors = SearchBarDefaults.colors(
-      inputFieldColors = SearchBarDefaults.inputFieldColors(
-        focusedPlaceholderColor = placeHolderColor,
-        unfocusedPlaceholderColor = placeHolderColor
-      )
-    ),
+    colors = colors,
     placeholder = { Text(text = "Search YouTube") },
     leadingIcon = {
-      IconButton(onClick = backPress) {
+      IconButton(onClick = onLeadingClick) {
         Icon(
-          imageVector = Icons.Filled.ArrowBack,
+          imageVector = Icons.Default.ArrowBack,
           modifier = Modifier,
-          contentDescription = ""
+          contentDescription = "Navigate back"
         )
       }
     },
     trailingIcon = {
-      IconButton(onClick = clearInput) {
+      IconButton(onClick = onTrailingClick) {
         Icon(
-          imageVector = Icons.Filled.Close,
+          imageVector = Icons.Default.Close,
           modifier = Modifier,
-          contentDescription = ""
+          contentDescription = "Clear search query"
         )
       }
     },
     onQueryChange = onQueryChange,
     onActiveChange = onActiveChange,
-    onSearch = onSearch
+    onSearch = onSearchClick
   ) {
     LazyColumn(modifier = Modifier.padding(top = 8.dp)) {
       itemsIndexed(
@@ -119,13 +123,13 @@ fun TySearchBar(
         items = suggestions
       ) { _, str ->
         SearchSuggestionItem(text = str) {
-          onSuggestionSelected(str)
+          onSuggestionClick(str)
         }
       }
     }
   }
 
-  LaunchedEffect(isActive) {
-    if (isActive) focusRequester.requestFocus()
+  LaunchedEffect(isSearchBarActive) {
+    if (isSearchBarActive) focusRequester.requestFocus()
   }
 }
